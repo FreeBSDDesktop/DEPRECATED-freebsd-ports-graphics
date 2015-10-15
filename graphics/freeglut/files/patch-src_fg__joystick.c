0509cfa2a15964213f8596d471cf8baaa061e23a
Niclas Zeising <zeising@FreeBSD.org>
2015-10-15 23:17:26 +0200

    Bump freeglut version to 3.0
    This only bumps the version, and makes the build use cmake instead of
    gnu configure.  More work are needed to actually build this, fix plist
    issues and so on.

--- graphics/freeglut/files/patch-src_fg__joystick.c
+++ graphics/freeglut/files/patch-src_fg__joystick.c
@@ -1,6 +1,6 @@
---- src/freeglut_joystick.c.orig	2011-09-28 03:37:26.000000000 +0200
-+++ src/freeglut_joystick.c	2012-01-05 09:51:03.000000000 +0100
-@@ -152,7 +152,10 @@
+--- src/fg_joystick.c.orig	2014-10-20 15:27:04 UTC
++++ src/fg_joystick.c
+@@ -60,7 +60,10 @@
  #                include <libusbhid.h>
  #            endif
  #        endif
@@ -12,7 +12,7 @@
  #        include <dev/usb/usbhid.h>
  
  /* Compatibility with older usb.h revisions */
-@@ -199,6 +202,7 @@
+@@ -107,6 +110,7 @@ struct os_specific_s {
   */
  static char *fghJoystickWalkUSBdev(int f, char *dev, char *out, int outlen)
  {
@@ -20,7 +20,7 @@
    struct usb_device_info di;
    int i, a;
    char *cp;
-@@ -220,6 +224,7 @@
+@@ -128,6 +132,7 @@ static char *fghJoystickWalkUSBdev(int f
          return out;
        }
    }
