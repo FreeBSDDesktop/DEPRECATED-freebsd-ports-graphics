e8978eaeea0141d7f45d6eb646e6634a95b019c1
Niclas Zeising <zeising@FreeBSD.org>
2015-10-16 13:22:00 +0200

    Refactor
    Move struct os_specific_s to after the definition of _JS_MAX_AXES, as
    the definition is used in the struct.
    
    XXX - Should this be moved to a *BSD-specific file?

--- graphics/freeglut/files/patch-src_x11_fg__internal__x11.h
+++ graphics/freeglut/files/patch-src_x11_fg__internal__x11.h
@@ -1,31 +1,33 @@
 --- src/x11/fg_internal_x11.h.orig	2015-10-16 10:42:49 UTC
 +++ src/x11/fg_internal_x11.h
-@@ -153,6 +153,28 @@ struct tagSFG_PlatformWindowState
- #        endif
- #        define JS_DATA_TYPE joystick
- #        define JS_RETURN (sizeof(struct JS_DATA_TYPE))
+@@ -189,6 +189,30 @@ struct tagSFG_PlatformWindowState
+  */
+ #    define _JS_MAX_AXES 16
+ typedef struct tagSFG_PlatformJoystick SFG_PlatformJoystick;
 +
++#    if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
 +struct os_specific_s {
 +  char             fname [128 ];
 +  int              fd;
 +  int              is_analog;
 +  /* The following structure members are specific to analog joysticks */
 +  struct joystick  ajs;
-+#    ifdef HAVE_USB_JS
++#      ifdef HAVE_USB_JS
 +  /* The following structure members are specific to USB joysticks */
 +  struct hid_item *hids;
 +  int              hid_dlen;
 +  int              hid_offset;
 +  char            *hid_data_buf;
 +  int              axes_usage [ _JS_MAX_AXES ];
-+#    endif
++#      endif
 +  /* We keep button and axes state ourselves, as they might not be updated
 +   * on every read of a USB device
 +   */
 +  int              cache_buttons;
 +  float            cache_axes [ _JS_MAX_AXES ];
 +};
++#    endif
 +
- #    endif
- 
- #    if defined(__linux__)
+ struct tagSFG_PlatformJoystick
+ {
+ #   if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
